f411c5a6e3de0129af126746713d35271a3442ca
RangerX <rangerx.1985@gmail.com>
2016-09-28 14:49:15 +0400

    * pinger class updated

--- lib/outpost/scouts/ping.rb
+++ lib/outpost/scouts/ping.rb
@@ -25,17 +25,31 @@ module Outpost
       # @param [Hash] Options to setup the scout
       # @option options [String] :host The host that will be "pinged".
       # @option options [Object] :pinger An object that can ping hosts.
+      # @option options [Integer] :count Count of ping attempts.
+      # @option options [Integer] :interval Interval (in sec) between ping attempts.
+      # @option options [Integer] :timeout Timeout (in sec) for waiting response.
       #   Defaults to Net::Ping::External.new
       def setup(options)
-        @host   = options[:host]
-        @pinger = options[:pinger] || Net::Ping::External.new
+        @host     = options[:host]
+        @pinger   = options[:pinger] || Net::Ping::External.new
+        @count    = options[:count] || 1
+        @interval = options[:interval] || 1
+        @timeout  = options[:timeout] || 5
       end
 
       # Runs the scout, pinging the host and getting the duration.
       def execute
-        if @pinger.ping(@host)
-          # Miliseconds
-          @response_time = @pinger.duration * 1000
+        if @pinger.is_a?(Net::Ping::External)
+          if @pinger.ping(@host, @count, @interval, @timeout)
+            # Miliseconds
+            @response_time = @pinger.duration * 1000
+          end
+        else
+          # another pinger. Just pass @host
+          if @pinger.ping(@host)
+            # Miliseconds
+            @response_time = @pinger.duration * 1000
+          end
         end
       end
     end
